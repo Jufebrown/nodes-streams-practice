28f8eb69c17291f476688e6acd630d15e5a8e1d9
Jufebrown <jufebrown@gmail.com>
2017-03-03 11:21:39 -0600

    worked with read, transform, and write

--- streams.js
+++ streams.js
@@ -1,18 +1,45 @@
 #!/usr/bin/env node
 
-const {createReadStream} = require('fs')
+// const {createReadStream} = require('fs')
 
-const readStream = createReadStream('names.txt')
+// const readStream = createReadStream('names.txt')
 
-readStream.pipe(process.stdout)
+// readStream.pipe(process.stdout)
 
-readStream.on('data', (buffer) => {
-  readStream.pause()
-  process.stdout.write(buffer.toString())
-})
+// readStream.on('data', (buffer) => {
+//   readStream.pause()
+//   process.stdout.write(buffer.toString())
+// })
 
-const timer = setInterval(() => readString.resume(), 1000)
+// const timer = setInterval(() => readString.resume(), 1000)
 
-readStream.on('end', () => {
-  clearInterval(timer)
-})
+// readStream.on('end', () => {
+//   clearInterval(timer)
+// })
+
+const {Readable, Writable, Transform} = require('stream')
+
+const readStream = Readable()
+const transformStream = Transform()
+const writeStream = Writable()
+
+let i =0
+
+readStream._read = () => {
+  if (i > 100) {
+    readStream.push(null)
+  } else {
+    setTimeout(() => readStream.push(`${i++}`), 50)
+  }
+}
+
+transformStream._transform = (buffer, encoding, done) => {
+  setTimeout(() => done(null, `${(Number(buffer)*2)}`), 1000)
+}
+
+writeStream._write = (buffer, encoding, done) => {
+  process.stdout.write(`${buffer}\n`)
+  setTimeout(done, 500)
+}
+
+readStream.pipe(transformStream).pipe(writeStream)
