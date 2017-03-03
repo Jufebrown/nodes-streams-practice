36510ce82c83585cad52eae1e847ace845cac780
Jufebrown <jufebrown@gmail.com>
2017-03-03 10:48:26 -0600

    added event handler

--- streams.js
+++ streams.js
@@ -5,3 +5,14 @@ const {createReadStream} = require('fs')
 const readStream = createReadStream('names.txt')
 
 readStream.pipe(process.stdout)
+
+readStream.on('data', (buffer) => {
+  readStream.pause()
+  process.stdout.write(buffer.toString())
+})
+
+const timer = setInterval(() => readString.resume(), 1000)
+
+readStream.on('end', () => {
+  clearInterval(timer)
+})
